ecac634a1efe5c9c63086f657de8726912859b13
Mu Kirgul <duzkar@gmail.com>
2020-12-04 21:31:21 -0500

    ProBuilder

--- Assets/Scripts/Car_Controller.cs
+++ Assets/Scripts/Car_Controller.cs
@@ -7,16 +7,18 @@ public class Car_Controller : MonoBehaviour
     public float forwardThrust = 100f;
     public float rotationPower = 1f;
     public float brakePower = 1f;
-    public float safeMaxHeight;
     public float rotationDampener;
     public float downForce; // when above max safe height
     public float downForceHeight; // when downforce needs to be applied
-    public float upForce; // to lift the car up
+    public BasicWheel[] wheels;
+    public float maxSteering;
     [Space]
     public GameObject leftHeadLight;
     public GameObject rightHeadLight;
-    float vertical, horizontal, velocity, trueVelocity, sidewaysVelocity;
-    bool isBreaking, isAtSafeHeight, lights, applyDownforce, inReverse;
+    float vertical, horizontal, velocity, trueVelocity, sidewaysVelocity, powerPerWheel;
+    bool isBreaking, lights, applyDownforce, inReverse;
+    int poweredWheelCount;
+    Transform _temp;
     RaycastHit hit, hit2;
 
     private void Start()
@@ -24,8 +26,26 @@ public class Car_Controller : MonoBehaviour
         if (!lights)
             lights = true;
         ToggleLights();
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            if (wheels[i]._isPowered)
+            {
+                poweredWheelCount++;
+            }
+        }
+        powerPerWheel = forwardThrust / poweredWheelCount;
     }
-
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            _temp = wheels[i]._transform;
+            if (_temp != null)
+            {
+                Gizmos.DrawWireSphere(_temp.position, wheels[i]._radius);
+            }
+        }
+    }
     private void Update()
     {
         vertical = Input.GetAxisRaw("Vertical");
@@ -35,12 +55,7 @@ public class Car_Controller : MonoBehaviour
         inReverse = trueVelocity < 0f;
         velocity = Mathf.Abs(trueVelocity);
         sidewaysVelocity = Mathf.Abs(transform.InverseTransformVector(Rb.velocity).x);
-        isAtSafeHeight = Physics.Raycast(transform.position, -transform.up, out hit, safeMaxHeight);
         applyDownforce = !Physics.Raycast(transform.position, -transform.up, out hit2, downForceHeight);
-        if (isAtSafeHeight)
-            Debug.DrawLine(transform.position, transform.position - transform.up * safeMaxHeight, Color.green);
-        else
-            Debug.DrawLine(transform.position, transform.position - transform.up * safeMaxHeight, Color.red);
 
         if (applyDownforce)
             Debug.DrawLine(transform.position, transform.position - transform.up * downForceHeight, Color.blue);
@@ -49,7 +64,7 @@ public class Car_Controller : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))//reset
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z);
+            //transform.position = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z);
             Vector3 _rotation = transform.rotation.eulerAngles;
             transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, 0);
             Rb.velocity = Vector3.zero;
@@ -60,13 +75,15 @@ public class Car_Controller : MonoBehaviour
         {
             ToggleLights();
         }
-
-        if (velocity > 0.1f)// reverse steering while driving backwards
+        for (int i = 0; i < wheels.Length; i++)
         {
-            if (inReverse)
-                transform.Rotate(transform.up, -horizontal * rotationPower * Time.deltaTime, Space.Self);
+            if (wheels[i]._isSteeringWheel)
+                wheels[i]._transform.localRotation = Quaternion.Euler(new Vector3(0, maxSteering * horizontal, 0));
+            if (wheels[i]._isPowered && vertical != 0)
+                wheels[i]._transform.Rotate(wheels[i]._transform.right, forwardThrust * vertical);
             else
-                transform.Rotate(transform.up, horizontal * rotationPower * Time.deltaTime, Space.Self);
+                wheels[i]._transform.Rotate(wheels[i]._transform.right, trueVelocity / wheels[i]._circumference * 360f);
+            wheels[i]._isGrounded = Physics.Raycast(wheels[i]._transform.position, -wheels[i]._transform.up, out hit, wheels[i]._radius);
         }
     }
 
@@ -87,24 +104,51 @@ public class Car_Controller : MonoBehaviour
                 Rb.angularVelocity = Vector3.Lerp(Rb.angularVelocity, Vector3.zero, brakePower);
             }
         }
-        else if (isAtSafeHeight)
+        else
         {
-            Rb.AddForce(
-                vertical *
-                transform.forward *
-                Time.deltaTime *
-                forwardThrust,
-                ForceMode.Acceleration
-            );
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                if (wheels[i]._isGrounded)
+                {
+                    Rb.AddForceAtPosition(
+                        vertical *
+                        transform.forward *
+                        Time.deltaTime *
+                        powerPerWheel,
+                        wheels[i]._transform.position,
+                        ForceMode.Acceleration
+                    );
+                }
+            }
         }
-
+        if (velocity > 0.1f)// reverse steering while driving backwards
+        {
+            if (inReverse)
+                Rb.AddRelativeTorque(transform.up * -horizontal * rotationPower * Time.deltaTime);
+            else
+                Rb.AddRelativeTorque(transform.up * horizontal * rotationPower * Time.deltaTime);
+        }
         if (applyDownforce)
         {
             Rb.AddForce(-Vector3.up * downForce);
         }
-        else
+    }
+    [Serializable]
+    public class BasicWheel
+    {
+        public Transform _transform;
+        public float _radius;
+        public bool _isPowered;
+        public bool _isSteeringWheel;
+        [HideInInspector]
+        public float _circumference;
+        public bool _isGrounded;
+
+        public BasicWheel(Transform transform, float radius)
         {
-            Rb.AddForce(Vector3.up * upForce);
+            _transform = transform;
+            _radius = radius;
+            _circumference = 2.0f * Mathf.PI * radius;
         }
     }
 }
