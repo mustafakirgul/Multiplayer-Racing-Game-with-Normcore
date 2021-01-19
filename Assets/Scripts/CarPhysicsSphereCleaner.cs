bf8701f6e85c8decc2982d01e2a8197b3d34eafd
Mu Kirgul <duzkar@gmail.com>
2021-01-19 14:45:27 -0500

    Podium for builds

---
+++ Assets/Scripts/CarPhysicsSphereCleaner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CarPhysicsSphereCleaner : MonoBehaviour
+{
+    Transform master;
+    private void Start()
+    {
+        if (master==null)
+        {
+            master = transform.parent;
+        }
+    }
+    private void Update()
+    {
+        if (master==null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PodiumController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PodiumController : MonoBehaviour
+{
+    public float defaultRotationSpeed = 1;
+    [SerializeField]
+    private bool isRotating;
+    Transform child => transform.GetChild(0);
+    float rotation;
+
+    // Start is called before the first frame update
+    public void StartRotation()
+    {
+        isRotating = true;
+    }
+
+    public void StopRotation()
+    {
+        isRotating = false;
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            rotation = (rotation + defaultRotationSpeed * Time.deltaTime) % 360f;
+            child.localEulerAngles = new Vector3(
+                0,
+                rotation,
+                0);
+        }
+    }
+}
