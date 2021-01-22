869850713c4618cb377b0f31ec08b477b4d3dff8
Mu Kirgul <duzkar@gmail.com>
2021-01-22 11:19:00 -0500

    network model modification

--- Assets/Scripts/Race.cs
+++ Assets/Scripts/Race.cs
@@ -1,48 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Normal.Realtime;
+﻿using Normal.Realtime;
 
 public class Race : RealtimeComponent<RaceModel>
 {
     public double m_fGameStartTime;
     public float m_fRaceDuration;
-    public RaceModel _model;
+
 
     protected override void OnRealtimeModelReplaced(RaceModel previousModel, RaceModel currentModel)
     {
         if (previousModel != null)
         {
-            // Unregister from events
             previousModel.gameStartTimeDidChange -= GameTimeChanged;
         }
 
         if (currentModel != null)
         {
             currentModel.gameStartTimeDidChange += GameTimeChanged;
-            //Debug.LogWarning("Model updated");
-
-            _model = currentModel;
-            m_fGameStartTime = _model.gameStartTime;
         }
-    }
-
-    private void Update()
-    {
-        //if (_model!=null)
-        //{
-        //    Debug.Log("GameStartTime: " + _model.gameStartTime);
-        //    Debug.Log("Model is " + _model);
-        //}
     }
 
     public void ChangeGameTime(double Time)
     {
-        _model.gameStartTime = Time;
+        model.gameStartTime = Time;
     }
 
     private void GameTimeChanged(RaceModel model, double value)
     {
-        m_fGameStartTime = value;
+        m_fGameStartTime = model.gameStartTime;
     }
 }
