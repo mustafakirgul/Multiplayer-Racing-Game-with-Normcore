ad2a6dcd0759f42df5b3bd5a1f9c72705d118871
Mu Kirgul <duzkar@gmail.com>
2021-01-22 21:36:37 -0500

    solution

--- Assets/Scripts/Weapons/WeaponProjectileBase.cs
+++ Assets/Scripts/Weapons/WeaponProjectileBase.cs
@@ -59,7 +59,6 @@ public class WeaponProjectileBase : RealtimeComponent<ProjectileModel>
                 }
 
                 isExploded = model.exploded;
-                StartCoroutine(HitCR());
             }
         }
     }
