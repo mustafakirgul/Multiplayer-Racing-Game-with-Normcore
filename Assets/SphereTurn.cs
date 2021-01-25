c351c4a1e30b9afde7ab6e67a28fd7f3f0aad97f
Mu Kirgul <duzkar@gmail.com>
2021-01-25 13:52:17 -0500

    Truck Damage Feedback

---
+++ Assets/SphereTurn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SphereTurn : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, Random.Range(0f, 360f));
+    }
+}
