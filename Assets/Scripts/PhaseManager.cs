ee3754197ab3aa3692e819b97fe6c9230517cad4
Mu Kirgul <duzkar@gmail.com>
2021-01-29 10:42:10 -0500

    Coroutines to Invokes in Phase Manager

--- Assets/Scripts/PhaseManager.cs
+++ Assets/Scripts/PhaseManager.cs
@@ -12,8 +12,8 @@ public class PhaseManager : MonoBehaviour
     public int phase;
     public TextMeshProUGUI messageTMPro;
     public UnityEvent endOfPhasesEvent;
-    private Coroutine timer, displayMessage;
-    private WaitForSeconds wait, waitForMessage;
+    private bool timer, displayMessage;
+    private float wait, waitForMessage;
 
     [SerializeField] private GameObject[] Walls;
 
@@ -26,8 +26,8 @@ public class PhaseManager : MonoBehaviour
     public void NextPhase() //called locally by the local game manager, depending on conditions
     {
         //Debug.LogWarning("Previous Phase: " + phase);
-        if(GameManager.instance.lootTruck.isOwnedLocallyInHierarchy)
-        phase++;
+        if (GameManager.instance.lootTruck.isOwnedLocallyInHierarchy)
+            phase++;
         if (phase == phases.Count)
         {
             endOfPhasesEvent.Invoke();
@@ -49,11 +49,8 @@ public class PhaseManager : MonoBehaviour
             phases[phase].startEvent.Invoke();
             //Debug.LogWarning("Start Event of Phase " + phase);
             if (phases[phase].startMessage.Length > 0)
-            {
-                if (displayMessage != null) StopCoroutine(displayMessage);
-                displayMessage = StartCoroutine(CR_DisplayMessage(phases[phase].startMessage));
-            }
 
+                DisplayMessage(phases[phase].startMessage);
             if (phases[phase].type == PhaseType.timeBased
             ) //if time based set up the timer - if not, just wait for someone to trigger the transition
                 StartTimer(phases[phase].duration);
@@ -62,24 +59,23 @@ public class PhaseManager : MonoBehaviour
 
     private void StartTimer(float duration)
     {
-        wait = new WaitForSeconds(duration);
-        if (timer != null) StopCoroutine(timer);
-        timer = StartCoroutine(CR_Timer());
+        Invoke("NextPhase", duration);
     }
 
-    IEnumerator CR_Timer()
+    private void DisplayMessage(string message)
     {
-        yield return wait;
-        NextPhase();
+        if (!displayMessage)
+        {
+            displayMessage = true;
+            messageTMPro.SetText(message);
+            Invoke("ClearMessage", message.Length * .1f);
+        }
     }
 
-    IEnumerator CR_DisplayMessage(string message)
+    private void ClearMessage()
     {
-        waitForMessage = new WaitForSeconds(message.Length * 0.1f);
-        messageTMPro.SetText(message);
-        yield return waitForMessage;
-        messageTMPro.SetText("");
-        yield return null;
+        messageTMPro.ClearMesh();
+        displayMessage = false;
     }
 }
 
