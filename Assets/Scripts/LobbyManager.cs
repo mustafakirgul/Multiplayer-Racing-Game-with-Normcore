7d121ed6755897a97896c2ec1f93c8e4379e1b31
Mu Kirgul <duzkar@gmail.com>
2021-02-03 23:24:59 -0500

    Lobby WIP

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    public static LobbyManager instance;
+    private Lobby lobby;
+    private Realtime _realtime => FindObjectOfType<Realtime>();
+    private Coroutine cr_RoomChecker;
+    private bool isConnectedToALobby;
+    public float serverCheckDelay;
+    private WaitForSeconds wait => new WaitForSeconds(serverCheckDelay);
+
+    private void Awake()
+    {
+        _realtime.didConnectToRoom += DidConnectToLobby;
+        _realtime.didDisconnectFromRoom += DidDisconnectFromLobby;
+    }
+
+    private void Start()
+    {
+        if (instance != null)
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        CheckRoom(roomName);
+    }
+
+    void CheckRoom(string roomName)
+    {
+        if (cr_RoomChecker == null)
+        {
+            cr_RoomChecker = StartCoroutine(CR_CheckRoom(roomName));
+        }
+    }
+
+    IEnumerator CR_CheckRoom(string roomName)
+    {
+        isConnectedToALobby = false;
+        _realtime.Connect(roomName);
+        while (!isConnectedToALobby)
+        {
+            //wait
+        }
+
+        yield return wait;
+        
+        
+    }
+
+    void DidConnectToLobby(Realtime realtime)
+    {
+        isConnectedToALobby = true;
+    }
+
+    void DidDisconnectFromLobby(Realtime realtime)
+    {
+        isConnectedToALobby = false;
+    }
+}
---
+++ Assets/Scripts/MoveSlowUIBG.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MoveSlowUIBG : MonoBehaviour
+{
+    private void Start()
+    {
+        LeanTween.scale(transform.GetComponent<RectTransform>(), new Vector3(1.25f, 1.12f, 1f), 60f)
+            .setEaseInOutBounce();
+    }
+}
