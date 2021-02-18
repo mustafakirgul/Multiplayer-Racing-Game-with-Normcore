db18e272927ef1f31346566c86edebaae1132a6b
Mu Kirgul <duzkar@gmail.com>
2021-02-18 12:28:51 -0500

    UI Loot Collection Prompt Init

---
+++ Assets/Scripts/LootCollectionFeedback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LootCollectionFeedback : MonoBehaviour
+{
+    private Text text;
+    private Image image;
+    private Animator[] animators;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        image = GetComponentInChildren<Image>();
+        animators = GetComponentsInChildren<Animator>();
+    }
+}
