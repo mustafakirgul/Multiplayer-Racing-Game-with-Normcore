9a3a3c6ba429ca6416401225ec20f448c7b6e844
Mu Kirgul <duzkar@gmail.com>
2021-02-27 12:16:36 -0500

    Minimap WIP

--- Assets/Scripts/MiniMapCamera.cs
+++ Assets/Scripts/MiniMapCamera.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class MiniMapCamera : MonoBehaviour
 {
     public Transform _master;
+    public float desiredHeight;
 
     private void Update()
     {
         if (_master != null)
         {
-            transform.position = new Vector3(_master.position.x, transform.position.y, _master.position.z);
+            transform.position =
+                new Vector3(_master.position.x, _master.position.y + desiredHeight, _master.position.z);
         }
     }
 }
--- Assets/Scripts/MiniMapSprite.cs
+++ Assets/Scripts/MiniMapSprite.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+﻿using Normal.Realtime;
+using UnityEngine;
 
 public class MiniMapSprite : MonoBehaviour
 {
     Transform _parent;
     Vector3 _origin;
+    [SerializeField] private SpriteRenderer local, network;
 
     void Awake()
     {
         _parent = transform.parent;
         _origin = transform.rotation.eulerAngles;
+        if (local == null) local = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        local.enabled = false;
+        if (network != null) network = transform.GetChild(1).GetComponent<SpriteRenderer>();
+        network.enabled = false;
+    }
+
+    private void Start()
+    {
+        local.enabled = _parent.GetComponent<RealtimeView>().isOwnedLocallyInHierarchy;
+        network.enabled = _parent.GetComponent<RealtimeView>().isOwnedRemotelyInHierarchy;
     }
 
     void Update()
