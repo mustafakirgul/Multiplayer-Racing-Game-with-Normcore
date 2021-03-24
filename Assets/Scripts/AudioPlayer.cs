b11cbfe366f683c80681d4f83d6ebb5df47ccb2a
Mu Kirgul <duzkar@gmail.com>
2021-03-24 09:52:43 -0400

    AudioPlayer component for objects that make a sound in the game.

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    AudioSource source => GetComponent<AudioSource>();
+    public AudioClip[] sounds;
+    public bool debug;
+
+    private void Start()
+    {
+        if (debug) PlaySound(0);
+    }
+
+    public void PlaySound(int index)
+    {
+        if (sounds != null)
+        {
+            if (index <= sounds.Length)
+            {
+                if (sounds[index] != null)
+                {
+                    source.clip = sounds[index];
+                    source.PlayOneShot(sounds[index]);
+                    return;
+                }
+            }
+        }
+
+        Debug.LogWarning("Sound " + index + " for " + transform.name + " has no audio clip attached!");
+    }
+}
