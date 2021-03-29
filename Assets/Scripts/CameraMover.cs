f18a6d30346f530e89f12744765159f05123ea94
MSI\Wu <zhangwu@sheridancollege.ca>
2021-03-29 15:28:12 -0400

    Pan Camera Tunings

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -48,7 +48,7 @@ public class CameraMover : MonoBehaviour
         float step = speed * Time.deltaTime; // calculate distance to move
         this.transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         lerpValue = (1 - Vector3.Distance(transform.position, target.position) / MarkDistance);
-        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, target.localRotation, lerpValue);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, target.localRotation, lerpValue * 0.75f);
     }
     void CheckDistance()
     {
