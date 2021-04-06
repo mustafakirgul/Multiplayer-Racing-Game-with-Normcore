1116fb123fb8fb49bc7aa97f6540c218cd832e86
MSI\Wu <zhangwu@sheridancollege.ca>
2021-04-06 18:22:40 -0400

    Loot Collection power up names, crosshair colour change

--- Assets/Scripts/LootCollectionFeedback.cs
+++ Assets/Scripts/LootCollectionFeedback.cs
@@ -21,7 +21,22 @@ public class LootCollectionFeedback : MonoBehaviour
     {
         if (isPlaying) return;
         text.text = _text;
-        image.texture = images[_id < 0 ? 0 : 1];
+
+        if (_id == -1)
+        {
+            image.texture = images[2];
+
+        }
+        else if (_id < 0)
+        {
+            image.texture = images[0];
+        }
+        else
+        {
+            image.texture = images[1];
+        }
+
+        //image.texture = images[_id < 0 ? (_id == 0 ? 2 : 0) : 1];
         isPlaying = true;
         for (int i = 0; i < animators.Length; i++)
         {
